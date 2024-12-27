b0adcc20e73ab39a0cf5b0f8c232ba060786d3e0
pratikreddy0 <pratikreddy01@gmail.com>
2024-12-27 18:47:16 +0530

    Update again test imports to correctly reference app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 import pytest
 import pandas as pd
 from app import load_data, preprocess_data, train_model_if_needed  # Import from app.py
